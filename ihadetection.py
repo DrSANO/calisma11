6090c80c94b5af2f13e3ccdef6054e50f6558fbb
DrSANO <115525581+DrSANO@users.noreply.github.com>
2024-01-21 22:54:45 +0300

    Add files via upload

---
+++ ihadetection.py
@@ -0,0 +1,34 @@
+from numba import jit,cuda
+from ultralytics import YOLO
+import cv2 as cv
+import numpy as np
+import cvzone
+import math
+import time
+
+model = YOLO("fwlastn.pt")
+cam=cv.VideoCapture("hareketli_ucak3.mp4")
+secc=0
+while True:
+    lastsec=time.time()
+    ret, gor = cam.read()
+    gor= cv.resize(gor, (1280,720))
+    sonuc= model(gor,stream=True)
+    for s in sonuc:
+        boxes=s.boxes
+        for box in boxes:
+            x1,y1,x2,y2=box.xyxy[0]
+            x1,y1,x2,y2=int(x1),int(y1),int(x2),int(y2)
+            cv.rectangle(gor,(x1,y1),(x2,y2),(255,0,0),3)
+            """conf=box.conf[0]
+            conf=round(conf,2)"""
+
+    fps=1/(lastsec-secc)
+    secc=lastsec
+    print("fps:",fps)
+    cv.imshow("sa",gor)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+        
+cam.release()
+cv.destroyAllWindows()
